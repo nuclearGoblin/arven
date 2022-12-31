7182dccb67e65e28106b56221da6d3be46849403
Kitty <kathryn.harris@ucdenver.edu>
2022-12-30 18:59:40 -0700

    Finish drafting pseudocode

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #Imports
 import pandas as pd
-ingredient_data = pd.read_csv("ingredients.csv"); seasosoning_data = pd.read_csv("seasonings.csv")
+from tkinter import *
+
+#Load Data
+ingredient_data = pd.read_csv("ingredients.csv"); seasoning_data = pd.read_csv("seasonings.csv")
+recipes = pd.read_csv("recipes.csv")
 
 #Constants
 min_ingredients = 1; max_ingredients = 6; max_seasonings = 4
@@ -8,49 +12,93 @@ level2 = 100; level3 = 2000
 
 #Variable Setup
 ingredients = []; seasonings = []
-num_players = 1
 powers = {'egg': 0, 'catching': 0, 'exp': 0, 'item': 0, 'raid': 0, 'title': 0, 'sparkling': 0, 'humungo': 0, 'teensy': 0, 'encounter': 0}
 flavors = {'sweet': 0, 'spicy': 0, 'bitter': 0, 'sour': 0, 'salty': 0}
 types = {'grass': 0, 'water': 0, 'fire': 0, 'dragon': 0, 'dark': 0, 'steel': 0, 'fairy': 0,
     'electric': 0, 'ice': 0, 'ghost': 0, 'psychic': 0, 'poison': 0, 'normal': 0, 'fighting': 0, 'rock': 0, 'ground': 0, 'bug': 0, 'flying': 0}
 #How are types matched to their associated powers?
 
-#Selection
+#Selection - structured based on https://www.geeksforgeeks.org/dropdown-menus-tkinter/
+playerdropdown = Tk()
+playerdropdown.geometry("200x200")
+playeroptions = [1,2,3,4]
+clickedplayers = IntVar()
+clickedplayers.set(1)
+dropplayers = OptionMenu(playerdropdown,clickedplayers, *playeroptions)
+dropplayers.pack()
+labelplayers = Label(playerdropdown,text = "Number of Players:")
+labelplayers.pack()
+playerdropdown.mainloop()
+num_players = playerdropdown.get()
 
-#Check selection validity
-if len(ingredients) < num_players * min_ingredients:
-    raise ValueError("You must select at least one ingredient per player.")
-elif (len(ingredients) > num_players * max_ingredients) or (len(seasonings) > num_players * max_seasonings):
-    raise ValueError("Too many ingredients or seasonings were selected. Please submit a bug report at https://github.com/nuclearGoblin/arven/issues.")
+for i in num_players*max_ingredients:
+    ingredients.append("")
+    ingredientdropdown = Tk()
+    ingredientdropdown.geometry("200x200")
+    ingredientoptions = ingredient_data['Name']
+    clickedingredient = IntVar()
+    clickedingredient.set(1)
+    dropingredients = OptionMenu(ingredientdropdown,clickedingredient, *ingredientoptions)
+    dropingredients.pack()
+    labelingredient = Label(ingredientdropdown,text = "Ingredient #"+str(i)+":")
+    labelingredient.pack()
+    ingredientdropdown.mainloop()
+    ingredients[i] = ingredientdropdown.get()
 
-#Sum up selection
-for x in ingredients + seasonings:
-    for y in powers + flavors + types:
-        y += x[y]
+def calculate():
+    #Remove blank entries
+    for i,x in enumerate(ingredients):
+        if x == "":
+            ingredients[i].delete()
+    for i,x in enumerate(seasonings):
+        if x == "":
+            seasonings[i].delete()
 
-#once quantifiable, program in reductions caused by the use of too many ingredients.
+    #Check selection validity
+    if (len(ingredients) < num_players * min_ingredients) or (len(seasonings) < num_players * min_ingredients):
+        raise ValueError("You must select at least one ingredient and one seasoning per player.")
+    elif (len(ingredients) > num_players * max_ingredients) or (len(seasonings) > num_players * max_seasonings):
+        raise ValueError("Too many ingredients or seasonings were selected. Please submit a bug report at https://github.com/nuclearGoblin/arven/issues.")
 
-#apply bonus from flavor
-#do bonuses for combinations only apply on a tie?
-if max(flavors) == 'sweet':
-    powers['egg'] += 100
-elif max(flavors) == 'spicy':
-    powers['humungo'] += 100
-elif max(flavors) == 'bitter':
-    powers['item'] += 100
-elif max(flavors) == 'sour':
-    powers['teensy'] += 100
-elif max(flavors) == 'salty':
-    powers['encounter'] += 100
-elif max(flavors) == ['sweet','sour']:
-    powers['catching'] += 100
-elif max(flavors) == ['salty','bitter']:
-    powers['exp'] += 100
-elif max(flavors) == ['spicy','sweet']:
-    powers['raid'] += 100
+    #Sum up selection
+    for x in ingredients + seasonings:
+        for y in powers + flavors + types:
+            y += (ingredient_data + seasoning_data)[x][y]
 
-#Check if sparkling power is permitted
-if sum([("Herba Mystica" in x) for x in ingredients]) < 2:
-    powers['sparkling'] = 0; #reset to 0 to force it not to appear.
+    #once quantifiable, program in reductions caused by the use of too many ingredients.
 
-#Get powers list
+    #apply bonus from flavor
+    #do bonuses for combinations only apply on a tie?
+    if max(flavors) == 'sweet':
+        powers['egg'] += 100
+    elif max(flavors) == 'spicy':
+        powers['humungo'] += 100
+    elif max(flavors) == 'bitter':
+        powers['item'] += 100
+    elif max(flavors) == 'sour':
+        powers['teensy'] += 100
+    elif max(flavors) == 'salty':
+        powers['encounter'] += 100
+    elif max(flavors) == ['sweet','sour']:
+        powers['catching'] += 100
+    elif max(flavors) == ['salty','bitter']:
+        powers['exp'] += 100
+    elif max(flavors) == ['spicy','sweet']:
+        powers['raid'] += 100
+
+    #Check if sparkling power is permitted
+    if sum([("Herba Mystica" in x) for x in ingredients]) < 2:
+        powers['sparkling'] = 0; #reset to 0 to force it not to appear.
+
+    #Get powers list
+    powers.sort()
+    types.sort()
+
+    #Check if recipe exists as exception
+    if [ingredients, seasonings] in recipes:
+        print(recipes[recipes == [ingredients,seasonings]][powers])
+    else: #Print powers list
+        for i in [1,2,3]:
+            print(powers[-i],types[-i])
+
+button = Button(playerdropdown, text = "Calculate", command = calculate).pack()
